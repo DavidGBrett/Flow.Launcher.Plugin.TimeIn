5539b8716b08bc00299a4cee4bd2a1ec8b35576e
David Brett <davidbrett2002@gmail.com>
2026-02-10 21:08:19 +0100

    Get list of timezones from Time.Now api

--- Flow.Launcher.Plugin.TimeIn/Main.cs
+++ Flow.Launcher.Plugin.TimeIn/Main.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Flow.Launcher.Plugin;
@@ -10,21 +12,58 @@ namespace Flow.Launcher.Plugin.TimeIn
     public class TimeIn : IAsyncPlugin
     {
         private PluginInitContext _context;
+        private HttpClient _httpClient;
+        private const string ApiBaseUrl = "https://time.now/developer/api/";
+
 
         public Task InitAsync(PluginInitContext context)
         {
             _context = context;
 
+            _httpClient = new HttpClient
+            {
+                Timeout = TimeSpan.FromSeconds(30)
+            };
+
             return Task.CompletedTask;
         }
 
-        public Task<List<Result>> QueryAsync(Query query, CancellationToken token)
+        public async Task<List<Result>> QueryAsync(Query query, CancellationToken token)
         {
             token.ThrowIfCancellationRequested();
 
             var results = new List<Result>{};
 
-            return Task.FromResult(results);
+            List<string> timezones = await GetTimezones(token);
+
+            token.ThrowIfCancellationRequested();
+
+            foreach (var timezone in timezones)
+            {
+                results.Add(new Result{
+                    Title = timezone,
+                }); 
+            }
+
+            return results;
+        }
+
+        public async Task<List<string>> GetTimezones(CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+
+            var fullUrl = ApiBaseUrl + "timezone";
+
+            
+            var response = await _httpClient.GetAsync(fullUrl,token);
+            response.EnsureSuccessStatusCode();
+
+            token.ThrowIfCancellationRequested();
+            
+            var responseBody = await response.Content.ReadAsStringAsync(token);
+
+            return JsonSerializer.Deserialize<List<string>>(responseBody);
+            
         }
     }
 }
--- Readme.md
+++ Readme.md
@@ -6,3 +6,6 @@ A plugin for the [Flow launcher](https://github.com/Flow-Launcher/Flow.Launcher)
 ### Usage
 
     ti <arguments>
+
+## Credits
+<a href="https://time.now">World Time API by Time.Now</a>
