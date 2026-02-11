be1a48fe62bf019679449319473cf639ea43fcb8
David Brett <davidbrett2002@gmail.com>
2026-02-11 14:52:07 +0100

    Add ability to add timezones to saved list

--- Flow.Launcher.Plugin.TimeIn/Main.cs
+++ Flow.Launcher.Plugin.TimeIn/Main.cs
@@ -37,7 +37,22 @@ namespace Flow.Launcher.Plugin.TimeIn
         {
             token.ThrowIfCancellationRequested();
 
-            var results = new List<Result>{};
+            var results = new List<Result>();
+
+            if (query.Search.StartsWith("add-")){
+                results = await GetAddTimezoneResults(token);
+            }
+            else{
+                results = await GetSavedTimezonesResults(token);
+            }
+
+            return results;
+        }
+
+        private async Task<List<Result>> GetSavedTimezonesResults(CancellationToken token){
+            token.ThrowIfCancellationRequested();
+
+            var results = new List<Result>();
 
             foreach (var timezone in _savedTimezones)
             {
@@ -45,6 +60,43 @@ namespace Flow.Launcher.Plugin.TimeIn
 
                 results.Add(new Result{
                     Title = $"{timezone} - {dateTime:HH:mm}",
+                }); 
+            }
+
+            results.Add(new Result{
+                Title = "Add Timezone",
+                Glyph = new GlyphInfo("sans-serif","＋"),
+                Score = -100, // Low score to appear at the bottom (make sure real matches come first)
+                Action = _ =>
+                {
+                    // Change to the add group query
+                    _context.API.ChangeQuery($"{_context.CurrentPluginMetadata.ActionKeyword} add-",false);
+                    return false;
+                }
+            });
+
+            return results;
+        }
+
+
+        private async Task<List<Result>> GetAddTimezoneResults(CancellationToken token){
+            token.ThrowIfCancellationRequested();
+
+            var results = new List<Result>();
+            
+            List<string> timezones = await GetTimezones(token);
+
+            token.ThrowIfCancellationRequested();
+
+            foreach (var timezone in timezones)
+            {
+                results.Add(new Result{
+                    Title = timezone,
+                    Action =  _ =>
+                    {
+                        _savedTimezones.Add(timezone);
+                        return false;
+                    }
                 }); 
             }
 
